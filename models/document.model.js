5a092e4a42a8a8bfeb321df0c1abf645fc220c1d
os-olga <olga.shcherbakova.cubex@gmail.com>
2023-05-21 22:32:56 +0300

    upd

--- models/document.model.js
+++ models/document.model.js
@@ -5,15 +5,15 @@ const Schema = mongoose.Schema;
 const documentSchema = new Schema(
   {
     userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-    birth_certificate: { type: String, required: true },
-    parent_passport: { type: String, required: true },
+    birth_certificate: { type: String, required: false },
+    parent_passport: { type: String, required: false },
     rnottac: { type: String, required: false }, //  Registration number of the taxpayer's account card
     certificate_of_residence_registration: { type: String, required: false },
     certificate_of_registration_of_person: { type: String, required: false },
     status: { type: String, enum: ["pending", "success"] },
   },
   {
-    timestamps: { createdAt: true, updatedAt: true },
+    timestamps: { created_at: true, updated_at: true },
   }
 );
 
